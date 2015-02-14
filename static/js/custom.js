20acc4ef35177949c25f2d30e5818deb995f3866
Rohit Prakash <rohitprakash@gmail.com>
2015-02-14 14:05:24 +0530

    select course
    TODO: fix on select bug

--- static/js/custom.js
+++ static/js/custom.js
@@ -23,6 +23,11 @@ $(function () {
     });
 });
 
+$(".dropdown-menu li a").click(function(){
+    $(this).parents(".btn-group").find('.selection').text($(this).text());
+    $(this).parents(".btn-group").find('.selection').val($(this).text());
+});
+
 $('#date').on('changeDate', function(ev){
     $(this).datepicker('hide');
 });
